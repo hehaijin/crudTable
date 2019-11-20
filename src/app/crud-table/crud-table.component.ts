9e047cbfd0e446da4cc94f02939d9b9863ab8166
He <hhe@tricore.org>
2019-11-20 16:12:55 -0700

    update

---
+++ src/app/crud-table/crud-table.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-crud-table',
+  templateUrl: './crud-table.component.html',
+  styleUrls: ['./crud-table.component.less']
+})
+export class CrudTableComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
